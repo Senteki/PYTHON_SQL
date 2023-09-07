a495e64df9fa88ae2a71c6ebc097ee3c82168cec
Senteki <141142881+Senteki@users.noreply.github.com>
2023-09-07 14:29:30 +0300

    Update create_order_test.py

--- create_order_test.py
+++ create_order_test.py
@@ -1,8 +1,27 @@
 import sender_stand_request
-import data
+
+# Cоздаем функцию заказа и получения его трека
+def get_track():
+    # В переменную response сохраняется результат запроса создания заказа
+    response = sender_stand_request.post_new_order()
+    # Успешное создание заказа
+    if response.status_code == 201:
+        return response.json()["track"]
 
 
-# запрос на получения заказа по треку
+    # Если заказ не создан выводим сообщение об ошибке
+    else:
+        print(f"Заказ не создан, код - {response.status_code}")
 
+# Функция получения заказа по его треку
+def test_order_by_track():
 
-# код ответа 200
+    # В track сохраняем трек заказа
+    track = get_track()
+    # В response сохраняем результат запроса на получение информации о заказе по треку
+    response = sender_stand_request.get_track_order(track)
+
+    # Проверяем код ответа: 200
+    assert response.status_code == 200
+    print(response)
+
